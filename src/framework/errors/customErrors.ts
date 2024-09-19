97eef005ccb4a1d9bd69f07a6f50361e7d164ace
Jansiya jahan <jansiyajahan8@gmail.com>
2024-09-19 11:17:01 +0530

    feat(erro) :  initialize custom errors for  handling error in frotend

---
+++ src/framework/errors/customErrors.ts
@@ -0,0 +1,58 @@
+
+
+import { CustomError } from "./customError";
+
+
+
+export class BadRequestError extends CustomError {
+    statusCode = 400;
+
+    constructor(public message: string) {
+        super(message);
+        Object.setPrototypeOf(this, BadRequestError.prototype);
+    }
+
+    serializeError() {
+        return [{ message: this.message }];
+    }
+}
+ 
+
+export class  InternalServerError extends CustomError{
+    statusCode = 500;
+
+    constructor(public message:string){
+        super(message);
+        Object.setPrototypeOf(this,InternalServerError.prototype);
+    }
+    serializeError() {
+        return [{ message: this.message }];
+    }
+    
+}
+
+export class UnauthorizedError extends CustomError {
+    statusCode = 401;
+
+    constructor(public message: string) {
+        super(message);
+        Object.setPrototypeOf(this, UnauthorizedError.prototype);
+    }
+
+    serializeError() {
+        return [{ message: this.message }];
+    }
+}
+
+export class InvalidTokenError extends CustomError {
+    statusCode = 403;
+
+    constructor(public message: string) {
+        super(message);
+        Object.setPrototypeOf(this, InvalidTokenError.prototype);
+    }
+
+    serializeError() {
+        return [{ message: this.message }];
+    }
+}
