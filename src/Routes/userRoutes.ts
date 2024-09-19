56a71c1993ac56ebfb79b0246efa211f89a0d90f
Jansiya jahan <jansiyajahan8@gmail.com>
2024-09-19 11:22:43 +0530

    feat :  initialize router for user controller

---
+++ src/Routes/userRoutes.ts
@@ -0,0 +1,19 @@
+
+
+import express, { Request, Response } from 'express';
+import { userController } from '../framework/utils/dependencyResolver';
+import { authenticateRefreshToken } from '../framework/middleware/tokenValidator';
+
+ export  const userRouter = express.Router();
+
+    
+    
+ userRouter.post('/register', (req: Request, res: Response) => userController.register(req, res));
+ userRouter.post('/verify-otp', (req: Request, res: Response) => userController.verifyOTP(req, res));
+ 
+ userRouter.post('/login', (req: Request, res: Response) => userController.login(req, res));
+
+ userRouter.post('/verify-token', authenticateRefreshToken, (req: Request, res: Response) => userController.verifyToken(req, res));
+ userRouter.post('/resend-otp',(req: Request, res: Response) => userController.resendOTP(req, res))
+userRouter.post('/logout',(req: Request, res: Response)=>userController.logout(req,res))
+   
