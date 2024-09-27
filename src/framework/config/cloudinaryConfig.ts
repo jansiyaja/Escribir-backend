86638c2cca6970ca8add1c18e0afdb8123b00b6c
Jansiya jahan <jansiyajahan8@gmail.com>
2024-09-27 23:49:10 +0530

    feat(user): implement Cloudinary config for uploading profile image

---
+++ src/framework/config/cloudinaryConfig.ts
@@ -0,0 +1,17 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+
+    max_image_width: 500,
+    max_image_height: 500,
+    eager: [
+      { width: 300, height: 300, crop: "pad" },
+      { width: 600, height: 600, crop: "pad" }
+    ]
+  });
+  
+export { cloudinary };
