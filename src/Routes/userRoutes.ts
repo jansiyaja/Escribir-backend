3995524c3179596bd45114e54500fd03200965f0
Jansiya jahan <jansiyajahan8@gmail.com>
2024-09-27 23:56:10 +0530

    feat(user): add new end point  for uploading profile image

--- src/Routes/userRoutes.ts
+++ src/Routes/userRoutes.ts
@@ -2,7 +2,8 @@
 
 import express, { Request, Response } from 'express';
 import { userController } from '../framework/utils/dependencyResolver';
-import { authenticateRefreshToken } from '../framework/middleware/tokenValidator';
+import { authenticateRefreshToken, authenticateToken } from '../framework/middleware/tokenValidator';
+import { uploadProfileImage } from '../framework/config/multerConfig';
 
  export  const userRouter = express.Router();
 
@@ -14,6 +15,11 @@ import { authenticateRefreshToken } from '../framework/middleware/tokenValidator
  userRouter.post('/login', (req: Request, res: Response) => userController.login(req, res));
 
  userRouter.post('/verify-token', authenticateRefreshToken, (req: Request, res: Response) => userController.verifyToken(req, res));
+
  userRouter.post('/resend-otp',(req: Request, res: Response) => userController.resendOTP(req, res))
-userRouter.post('/logout',(req: Request, res: Response)=>userController.logout(req,res))
-   
+ userRouter.post('/logout',(req: Request, res: Response)=>userController.logout(req,res))
+ 
+
+ // after authentication
+ userRouter.post('/profileImage',uploadProfileImage,authenticateToken,(req:Request,res:Response)=>userController.profileImageUpload(req,res));
+ 
