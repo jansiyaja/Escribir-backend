2b0f57f313272974288614cc3015c780ef41d17d
Jansiya jahan <jansiyajahan8@gmail.com>
2024-10-05 15:45:24 +0530

    feat(admin) : add new role feild for admin

---
+++ src/framework/models/admin.ts
@@ -0,0 +1,22 @@
+
+import mongoose, { Schema, Model } from 'mongoose';
+import { IAdmin } from '../../entities/Admin';
+
+const AdminSchema: Schema = new Schema({
+  email: { type: String },
+  password: { type: String },
+  isVerified: {
+    type: Boolean,
+    default: false, 
+  },
+  role: 'admin',
+},
+{
+    timestamps: true, 
+  }
+);
+
+const AdminModel = mongoose.model<IAdmin>('Admin', AdminSchema);
+
+export default AdminModel;
+
