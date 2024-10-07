cb53b1c212add9e2b56beeb4b2d5852f7dc49e93
Jansiya jahan <jansiyajahan8@gmail.com>
2024-10-07 10:13:40 +0530

    update(tag) update the adminRepository to do the tag crud Operation

--- src/interfaces/repositories/IAdminRepository.ts
+++ src/interfaces/repositories/IAdminRepository.ts
@@ -1,10 +1,21 @@
+import { ObjectId } from "mongoose";
 import { IAdmin } from "../../entities/Admin";
 import { IUser } from "../../entities/User";
+import { ITag } from "../../entities/ITag";
  
 
 
 export interface IAdminRepository{
     findByEmail(email: string): Promise<IAdmin | null>;
+    findById(id:ObjectId):Promise<IAdmin|null>;
     saveAdmin(adminData: Partial<IAdmin>): Promise<IAdmin>;
     getAllUsers(): Promise<IUser[]>;
+//-----------------------------------------------------------------------------------//
+    create(tagData: Partial<ITag>): Promise<ITag> ,
+    findOne(name: string): Promise<ITag | null> ;
+    findAll(): Promise<ITag[]>;
+    updateTagById(tagId: string, tagData: { name: string }): Promise<ITag> ;
+    deleteTagById(tagId: string): Promise<ITag> 
+
+ //-----------------------------------------------------------------------------------//
 }
