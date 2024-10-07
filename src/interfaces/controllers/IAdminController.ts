fc143af617226cd92cc415624c829978e52ca483
Jansiya jahan <jansiyajahan8@gmail.com>
2024-10-07 10:10:14 +0530

    feat(tag) add  new controller interface for  the crud operation for the tag in adminController

--- src/interfaces/controllers/IAdminController.ts
+++ src/interfaces/controllers/IAdminController.ts
@@ -3,6 +3,14 @@ import { Request,Response } from "express";
 export  interface  IAdminController{
     login(req: Request, res: Response): Promise<Response>;
     logout(req: Request, res: Response): Promise<Response> ;
+
+    verifyToken(req: Request, res: Response): Promise<Response>;
     listUsers(req: Request, res: Response): Promise<Response>
+    blockUser(req: Request, res: Response): Promise<Response>
+    unBlockUser(req: Request, res: Response): Promise<Response>;
+    createTag(req: Request, res: Response): Promise<Response>;
+    listTags(req: Request, res: Response): Promise<Response>;
+    updateTag(req: Request, res: Response): Promise<Response>;
+    deleteTag(req: Request, res: Response): Promise<Response> 
     
 }
