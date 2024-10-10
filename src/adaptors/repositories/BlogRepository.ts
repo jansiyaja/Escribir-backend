13a6d9de01d101678973f5d44824c6a93a9a19c3
Jansiya jahan <jansiyajahan8@gmail.com>
2024-10-10 15:05:13 +0530

    feat(blog repo) :add new  repository in post for updating and deleting the  post

--- src/adaptors/repositories/BlogRepository.ts
+++ src/adaptors/repositories/BlogRepository.ts
@@ -1,6 +1,9 @@
-import { IBlogPost } from "../../entities/Blog.Post";
+import { BlogStatus, IBlogPost } from "../../entities/Blog.Post";
 import { IBlogRepository } from "../../interfaces/repositories/IBlogRepository";
 import BlogPost from "../../framework/models/blog";
+import { ITag } from "../../entities/ITag";
+import TagModel from "../../framework/models/tag";
+import { ObjectId } from "mongoose";
 
 export class BlogRepositroy implements IBlogRepository{
     async create(blogPostData: Partial<IBlogPost>): Promise<IBlogPost> {
@@ -8,18 +11,39 @@ export class BlogRepositroy implements IBlogRepository{
         return await blogPost.save();
     }
     async findAll(): Promise<IBlogPost[]> {
-        return await BlogPost.find().sort({ createdAt: -1 }); 
+        const blogs = await BlogPost.find({ status:BlogStatus.PUBLISHED })
+        .populate('author_id', 'username image')
+        .sort({ createdAt: -1 });
+    return blogs
     }
 
-    // async findById(id: string): Promise<IBlogPost | null> {
-    //     return await BlogPost.findById(id);
-    // }
 
-    // async update(id: string, blogPostData: Partial<Omit<IBlogPost, '_id' | 'createdAt' | 'updatedAt'>>): Promise<IBlogPost | null> {
-    //     return await BlogPost.findByIdAndUpdate(id, blogPostData, { new: true });
-    // }
+    async findById(id: string): Promise<IBlogPost | null> {
+        return await BlogPost.findById(id)
+        .populate('author_id', 'username image');
+    }
+    async update(id: string, blogPostData: Partial<IBlogPost>): Promise<IBlogPost | null> {
+      
+        const updatedPost = await BlogPost.findByIdAndUpdate(id, blogPostData, { new: true });
+        if (!updatedPost) {
+            throw new Error('Blog not found'); 
+        }
+        return updatedPost;
+    }
+    
 
-    // async delete(id: string): Promise<IBlogPost | null> {
-    //     return await BlogPost.findByIdAndDelete(id);
-    // }
+    async delete(id: string): Promise<IBlogPost | null> {
+        return await BlogPost.findByIdAndDelete(id);
+    }
+
+    async findAllTags(): Promise<ITag[]> {
+        return await TagModel.find().sort({ createdAt: -1 }); 
+    }
+    async findByUserId(authorId: string): Promise<IBlogPost[] | null> {
+        return await BlogPost.find({ author_id: authorId })
+        .populate("author_id", "username image") 
+        .exec();
+        
+      }
+      
 }
