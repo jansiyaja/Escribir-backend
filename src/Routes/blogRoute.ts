3a07f8c73171813099f5b98bc3c65522ee6edcc8
Jansiya jahan <jansiyajahan8@gmail.com>
2024-10-10 15:12:38 +0530

    feat(blog route) :add new  route  for updating and deleting the  post

--- src/Routes/blogRoute.ts
+++ src/Routes/blogRoute.ts
@@ -7,5 +7,16 @@ export  const blogRouter = express.Router();
 
 blogRouter.post('/blogeditor',uploadBlogData,authenticateToken,(req:Request,res:Response)=>blogController.createBlogPost(req,res));
 blogRouter.post('/blogimage',uploadBlogImage,authenticateToken,(req:Request,res:Response)=>blogController.blogEditorImage(req,res));
-blogRouter.get('/',(req: Request, res: Response) => blogController.listBlogs(req, res))
+blogRouter.get('/',authenticateToken,(req: Request, res: Response) => blogController.listBlogs(req, res))
+blogRouter.get('/tags',authenticateToken,(req: Request, res: Response) => blogController.listTags(req, res))
+blogRouter.get('/singleblog/:id', authenticateToken, (req: Request, res: Response) => blogController.singleBlog(req, res));
+blogRouter.get('/getblog/:id', authenticateToken, (req: Request, res: Response) => blogController.singleBlogEdit(req, res));
+blogRouter.delete('/delete-tag/:id', authenticateToken, (req: Request, res: Response) => blogController.deletePost(req, res));
+blogRouter.put('/blogeditor/:id', authenticateToken, (req: Request, res: Response) => blogController.updatePost(req, res));
+
+
+blogRouter.get('/your-blog/:userId/', authenticateToken, (req: Request, res: Response) => blogController.userBlog(req, res));
+
+
+
 
