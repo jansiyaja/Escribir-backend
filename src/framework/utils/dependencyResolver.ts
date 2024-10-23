32e6b432007661c99750fc3bad7e215dfeaa8ccf
Jansiya jahan <jansiyajahan8@gmail.com>
2024-10-23 20:32:45 +0530

    add dependency resolvers for admina nd blog

--- src/framework/utils/dependencyResolver.ts
+++ src/framework/utils/dependencyResolver.ts
@@ -1,7 +1,18 @@
+import { AdminController } from "../../providers/controllers/adminController";
+import { BlogController } from "../../providers/controllers/blogController";
 import { UserController } from "../../providers/controllers/userController";
+import { AdminRepository } from "../../providers/repositories/adminRepository";
+import { BlogRepositroy } from "../../providers/repositories/blogRepository";
+import { CommentRepository } from "../../providers/repositories/commentRepository";
+import FollowRepository from "../../providers/repositories/followRepository";
+import NotificationRepository from "../../providers/repositories/notificationRepository";
 import { OTPVerificationRepository } from "../../providers/repositories/OTPRepository";
+import ReactionRepository from "../../providers/repositories/reactionRepository";
 import { UserRepository } from "../../providers/repositories/userRepository";
+import { AdminUseCase } from "../../useCases/AdminUseCase";
+import { BlogPostUseCase } from "../../useCases/BlogUseCase";
 import { UserUseCase } from "../../useCases/UserUseCase";
+import { s3 } from "../config/awsConfig";
 import { BcryptHashService } from "../services/hashService";
 import { SMTPService } from "../services/smtpService";
 
@@ -9,15 +20,28 @@ const userRepository=new UserRepository()
 const hashService = new  BcryptHashService()
 const emailService = new SMTPService();
 const otpRepository = new OTPVerificationRepository()
+const adminRepository = new AdminRepository()
+const blogRepository =new BlogRepositroy()
+const reactionReopository = new ReactionRepository() 
+const notificationRepository = new  NotificationRepository()
+const followerRepository = new FollowRepository()
+const commentRepository = new CommentRepository()
 
 
-
-
-const userUseCases = new UserUseCase(userRepository, hashService, emailService, otpRepository);
+const userUseCases = new UserUseCase(userRepository, hashService, emailService, otpRepository,followerRepository,notificationRepository);
 const userController = new UserController(userUseCases)
+
+
+const adminUseCase= new AdminUseCase(adminRepository,hashService ,userRepository);
+const adminController = new AdminController(adminUseCase);
+
+const blogUseCase= new BlogPostUseCase(blogRepository,s3,reactionReopository ,notificationRepository,commentRepository);
+const blogController = new BlogController(blogUseCase)
 
 export {  
     userController,
+    adminController,
+    blogController
    
 
 }
