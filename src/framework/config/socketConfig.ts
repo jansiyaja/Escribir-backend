41b599bce162648a17ea21824cd97c596539037c
Jansiya jahan <jansiyajahan8@gmail.com>
2024-10-29 23:14:20 +0530

    feat (webRtc) : implement webRtc for  real time streaming with socket io

--- src/framework/config/socketConfig.ts
+++ src/framework/config/socketConfig.ts
@@ -1,82 +1,143 @@
-import { Server } from 'http';
-import { Server as SocketIo } from 'socket.io';
-import { IUser } from '../../entities/User';
-const userSockets: Map<string, { socketId: string, userDetails: IUser }> = new Map();
+import { Server } from "http";
+import { Server as SocketIo } from "socket.io";
+import { IUser } from "../../entities/User";
 
+interface CallPayload {
+  from: string;
+  offer: RTCSessionDescriptionInit;
+  callType: "audio" | "video";
+}
+interface IceCandidatePayload {
+  candidate: RTCIceCandidateInit;
+  receiverId: string;
+}
+
+const userSockets: Map<string, { socketId: string; userDetails: IUser }> =
+  new Map();
+const messageNotificationCounts = new Map();
 const reactionEmojis: Record<string, string> = {
-  Like: '👍',
-  Happy: '😄',
-  Sad: '😢',
-  Angry: '😡',
-  Love:'😍'
+  Like: "👍",
+  Happy: "😄",
+  Sad: "😢",
+  Angry: "😡",
+  Love: "😍",
 };
 
-
 export const setupSocket = (server: Server) => {
-    const io = new SocketIo(server, {
-        cors: {
-            origin: process.env.FRONTEND_URL || 'http://localhost:5000',
-            methods: ['GET', 'POST'],
-            credentials: true,
-        },
+  const io = new SocketIo(server, {
+    cors: {
+      origin: process.env.FRONTEND_URL || "http://localhost:5000",
+      methods: ["GET", "POST"],
+      credentials: true,
+    },
+  });
+
+  io.on("connection", (socket) => {
+    socket.on("login", (userDetails) => {
+      const { userId } = userDetails;
+      userSockets.set(userId, { socketId: socket.id, userDetails });
     });
 
-    io.on('connection', (socket) => {
-        socket.on('login', (userDetails) => {
-            const { userId } = userDetails;
-            userSockets.set(userId, { socketId: socket.id, userDetails });
-          
+    socket.on("reaction", ({ userId, postAuthorId, reactionType }) => {
+      const postAuthorSocket = userSockets.get(postAuthorId);
+      const userDetails = userSockets.get(userId)?.userDetails;
+
+      if (postAuthorSocket) {
+        const emoji = reactionEmojis[reactionType.toString()] || "";
+
+        io.to(postAuthorSocket.socketId).emit("recive_Reaction", {
+          message: ` ${
+            userDetails?.username || userId
+          } reacted to your post with ${emoji}`,
+          reactionType: emoji,
+          user: userDetails,
         });
-
-
-       
-
-        socket.on('reaction', ({ userId, postAuthorId, reactionType }) => {
-
-
-            const postAuthorSocket = userSockets.get(postAuthorId);
-            const userDetails = userSockets.get(userId)?.userDetails;
-        
-            if (postAuthorSocket) {
-                const emoji = reactionEmojis[reactionType.toString()] || ''; 
-                
-                io.to(postAuthorSocket.socketId).emit('recive_Reaction', {
-                    message: ` ${userDetails?.username || userId} reacted to your post with ${emoji}`,
-                    reactionType: emoji,
-                    user: userDetails 
-                });
-            } else {
-                console.log(`Post author ${postAuthorId} is not currently connected.`);
-            }
-        });
-
-         socket.on('follow', ({ followerId, followedId }) => {
-            
-
-            const followedSocket = userSockets.get(followedId);
-            const followerDetails = userSockets.get(followerId)?.userDetails;
-
-            if (followedSocket) {
-                io.to(followedSocket.socketId).emit('recive_Reaction', {
-                    message: `${followerDetails?.username || followerId} has Requested to Follow You!`,
-                    follower: followerDetails 
-                });
-                console.log(`Notification sent to ${followedId} that ${followerId} followed them.`);
-            } else {
-                console.log(`User ${followedId} is not currently connected.`);
-            }
-        });
-
-
-        socket.on('disconnect', () => {
-            userSockets.forEach((value, key) => {
-                if (value.socketId === socket.id) {
-                    userSockets.delete(key);
-                    console.log(`User ${key} disconnected and removed from socket mapping`);
-                }
-            });
-        });
+      } else {
+        console.log(`Post author ${postAuthorId} is not currently connected.`);
+      }
     });
 
-    return io;
+    socket.on("follow", ({ followerId, followedId }) => {
+      const followedSocket = userSockets.get(followedId);
+      const followerDetails = userSockets.get(followerId)?.userDetails;
+
+      if (followedSocket) {
+        io.to(followedSocket.socketId).emit("recive_Reaction", {
+          message: `${
+            followerDetails?.username || followerId
+          } has Requested to Follow You!`,
+          follower: followerDetails,
+        });
+        console.log(
+          `Notification sent to ${followedId} that ${followerId} followed them.`
+        );
+      } else {
+        console.log(`User ${followedId} is not currently connected.`);
+      }
+    });
+
+    socket.on("send_message", ({ message, receiverId }) => {
+      const receiverSocket = userSockets.get(receiverId);
+      if (receiverSocket) {
+        io.to(receiverSocket.socketId).emit("receive_message", message);
+
+        const currentCount = messageNotificationCounts.get(receiverId) || 0;
+        messageNotificationCounts.set(receiverId, currentCount + 1);
+        console.log("count", messageNotificationCounts);
+
+        io.to(receiverSocket.socketId).emit(
+          "notification_count",
+          messageNotificationCounts.get(receiverId)
+        );
+      } else {
+        console.log(
+          `Notification sent to ${receiverId} that ${messageNotificationCounts} followed them.`
+        );
+      }
+    });
+
+    socket.on("call-user", ({ receiverId, offer, callType }) => {
+      const receiverSocket = userSockets.get(receiverId);
+      if (receiverSocket) {
+        io.to(receiverSocket.socketId).emit("receive-call", {
+          from: receiverId,
+          offer,
+          callType,
+        });
+      } else {
+        console.log(`Receiver ${receiverId} is not connected`);
+      }
+    });
+    socket.on("answer-call", ({ from, answer, callType }) => {
+      if (!answer) {
+        console.error("Error: Missing answer in 'answer-call' event.");
+        return;
+      }
+
+      socket.to(from).emit("call-answered", { answer });
+    });
+
+    socket.on(
+      "ice-candidate",
+      ({ receiverId, candidate }: IceCandidatePayload) => {
+        const receiverSocket = userSockets.get(receiverId);
+        if (receiverSocket && candidate) {
+          io.to(receiverSocket.socketId).emit("ice-candidate", { candidate });
+        }
+      }
+    );
+
+    socket.on("disconnect", () => {
+      userSockets.forEach((value, key) => {
+        if (value.socketId === socket.id) {
+          userSockets.delete(key);
+          console.log(
+            `User ${key} disconnected and removed from socket mapping`
+          );
+        }
+      });
+    });
+  });
+
+  return io;
 };
