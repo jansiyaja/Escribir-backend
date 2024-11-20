23010e54e160191b5ec247d3a5d281f0476d5680
Jansiya jahan <jansiyajahan8@gmail.com>
2024-11-20 11:11:53 +0530

    fixing  the   error in cors

--- src/app.ts
+++ src/app.ts
@@ -14,11 +14,15 @@ const createApp = () => {
 
   app.use(express.json());
   app.use(cookieParser());
-  app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-next() 
-})
+  app.use(
+    cors({
+      origin: [
+        "https://escribir-frontend-xtb3.vercel.app",
+        "http://localhost:5000",
+      ],
+      credentials: true,
+    })
+  );
 
   app.use("/users", () => console.log(), userRouter);
   app.use("/admin", adminRouter);
